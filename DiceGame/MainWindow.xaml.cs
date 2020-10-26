40c0e69af06e1fdfec8bdb8adecd35635081f8ba
sylgeek <psguillaume@outlook.com>
2020-10-26 18:46:55 +0400

    first commit

---
+++ DiceGame/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiceGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Random rd = new Random();
+            int num1 = rd.Next(1, 7);
+            textblock.Text = Convert.ToString(num1);
+
+            string route = Convert.ToString(AppDomain.CurrentDomain.BaseDirectory) + "/images/" + Convert.ToString(num1) + ".png";
+
+            Uri imguri = new Uri(route);
+            BitmapImage img = new BitmapImage(imguri);
+
+            popo.Source = img;
+            
+        }
+    }
+}
